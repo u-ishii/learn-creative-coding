1fd7af14c9d3656f03a1429d253fb2f6f1a2d9ea
u-ishii <yuki.ishii@ghelia.com>
2022-03-07 20:14:42 +0900

    vue init

--- app.js
+++
@@ -1,4 +0,0 @@
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    circle(width / 2, height / 2, 50);
-}
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  dummy!
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  mounted() {
+    console.log();
+  },
+});
+</script>
+
+<style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+}
+body {
+  overflow: hidden;
+  background: #292a33;
+}
+</style>
