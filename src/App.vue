39c0684b50cccd1c0e7e5a9266c94cc8a47372c0
u-ishii <yuki.ishii@ghelia.com>
2022-03-07 20:32:18 +0900

    install p5

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
-  dummy!
+  <div ref="canvas" class="canvas"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import P5 from 'p5';
+
+const initializeP5 = (p: P5) => {
+  /* eslint-disable no-param-reassign */
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.circle(p.width / 2, p.height / 2, 50);
+  };
+  /* eslint-enable no-param-reassign */
+};
 
 export default defineComponent({
   name: 'App',
   mounted() {
-    console.log();
+    // eslint-disable-next-line no-new
+    new P5(initializeP5, this.$refs.canvas as HTMLElement);
   },
 });
 </script>
@@ -18,7 +29,10 @@ html, body {
   margin: 0;
   padding: 0;
 }
-body {
+</style>
+
+<style lang="scss" scoped>
+.canvas {
   overflow: hidden;
   background: #292a33;
 }
