a805f294bfce3b11642cd9b7309006aeb6af1c67
u-ishii <yuki.ishii@ghelia.com>
2022-03-22 18:11:27 +0900

    fix deadend

--- src/utils/dfs-solver.ts
+++ src/utils/dfs-solver.ts
@@ -3,17 +3,21 @@ import { addPositions, DIRECTIONS } from '@/utils/position-calculator';
 import { generate2dArray } from './array-generator';
 
 export const solveDfs = (
-  (maze: Maze, startPosition: Position, goalPosition: Position): Position[] => {
+  (maze: Maze, startPosition: Position, goalPosition: Position): Position[][] => {
     const visitedMaze: boolean[][] = generate2dArray(maze.length, maze[0].length, false);
-    const loop = (current: Position, depth: number): Position[] => {
+    const loop = (current: Position): Position[][] => {
       visitedMaze[current.y][current.x] = true;
       if (visitedMaze[goalPosition.y][goalPosition.x]) return [];
       const aroundPositions = DIRECTIONS
         .map((direction) => addPositions(current, direction))
         .filter((position) => maze[position.y][position.x] === 'floor')
         .filter((position) => !visitedMaze[position.y][position.x]);
-      return [current, ...aroundPositions.flatMap((position) => loop(position, depth + 1))];
+      if (aroundPositions.length === 0) {
+        return [[current]];
+      }
+      return aroundPositions
+        .map((position) => [current, ...loop(position).flat()]);
     };
-    return loop(startPosition, 0);
+    return loop(startPosition);
   }
 );
