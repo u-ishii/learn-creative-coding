516e72eabe699916440ff789017d7a2a39c2b104
u-ishii <yuki.ishii@ghelia.com>
2022-04-19 16:00:06 +0900

    rename

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Maze from '@/views/Maze.vue';
+import Way from '@/views/Way.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Maze',
     component: Maze,
+  },
+  {
+    path: '/way',
+    name: 'Way',
+    component: Way,
   },
 ];
 
